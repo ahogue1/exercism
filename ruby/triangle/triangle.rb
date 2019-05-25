3af82c1463c558b34c93b5b803d58a6d011068cd
Amy Hogue <hogue.a@gmail.com>
2019-05-24 17:32:17 -0700

    complete triangle exercise

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -1,12 +1,29 @@
 class Triangle
 
-  def intitialize(sides)
+  def initialize(sides)
     @sides = sides
   end
 
-  def self.new(sides)
+  def triangle_inequality?
+    sum = @sides.sum
+
+    @sides.all? {|side| sum - side >= side }
   end
 
-  def self.equilateral?
+  def equilateral?
+    @sides.uniq.length == 1 && @sides.none? { |side| side == 0 }
   end
+
+  def two_sides_equal?
+    @sides.uniq.length <= 2
+  end
+
+  def isosceles?
+    triangle_inequality? && two_sides_equal?
+  end
+
+  def scalene?
+    @sides.uniq.length == 3 && triangle_inequality?
+  end
+
 end
