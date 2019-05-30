55cfb86e8d5832c9946e0b89eeb23bcf3ad50ec6
Amy Hogue <hogue.a@gmail.com>
2019-05-30 10:12:27 -0700

    refactor using #zip method for arrays

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,8 +2,7 @@ class Hamming
 
   def self.compute(strand1, strand2)
     raise ArgumentError, 'DNA strands must be equal lengths' unless strand1.length == strand2.length
-
-    strand1.chars.each_with_index.count { |nucl, index| nucl != strand2.chars[index] }
+    strand1.chars.zip(strand2.chars).count { |nucl1, nucl2| nucl1 != nucl2 }
   end
 
 end
