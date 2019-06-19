f8cc4912edb44be5e97ae28756c9c815cd00edff
Amy Hogue <hogue.a@gmail.com>
2019-06-19 10:04:54 -0700

    Refactor code based on mentor feedback

--- ruby/isogram/isogram.rb
+++ ruby/isogram/isogram.rb
@@ -1,7 +1,7 @@
 class Isogram
 
   def self.isogram?(input)
-    string = input.downcase.scan(/[a-z]/).join
-    string.each_char.uniq.length == string.length
+    string = input.downcase.scan(/[a-z]/)
+    string.uniq.length == string.length
   end
 end
