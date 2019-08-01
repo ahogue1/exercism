fc3171011317c82713aa770062ae8cf7e5bb8535
Amy Hogue <hogue.a@gmail.com>
2019-07-31 22:04:18 -0700

    Completed luhn test problem on Ruby track

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,17 +1,26 @@
 class Luhn
 
+  def self.luhn_number(num, index)
+    if index.odd?
+      num *= 2
+      num -= 9 if num > 9
+    end
+
+    num
+  end
+
   def self.valid?(number)
-    number_to_test = number.chars.map.with_index { | n, i|
-      if i.even?
-        doubled = n.to_i * 2
-        if doubled > 9
-          doubled = doubled - 9
-        end
-        doubled
-      else n.to_i
-      end
+    number_array = number.reverse.scan(/[0-9]/)
+
+    if number_array.length <= 1 || number_array.count != number.delete(' ').length
+      return false
+    end
+
+    number_to_test = number_array.map(&:to_i).map.with_index { | n, i|
+      Luhn.luhn_number(n, i)
     }.sum
 
-    number.length > 1 && number_to_test % 10 == 0
+   number_to_test % 10 == 0
   end
+
 end
